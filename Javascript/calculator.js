baeda22346fc8102fc6616f0dd20953606a1f967
mahdi <bitsgenix@gmail.com>
2024-10-17 13:18:23 +0330

    feat(calculator): implement basic arithmetic operations
    - Added functionality to perform addition, subtraction, multiplication, and division.
    - Implemented input handling using the readline module for user interaction.
    - Enhanced user experience by prompting for numbers and operations in a structured manner.

---
+++ Javascript/calculator.js
@@ -0,0 +1,33 @@
+const { createInterface } = require("readline/promises");
+function doTheMath(f, o, s) {
+  switch (o) {
+    case "*":
+      output = parseFloat(f) * parseFloat(s);
+      break;
+
+    case "/":
+      output = parseFloat(f) / parseFloat(s);
+      break;
+    case "+":
+      output = parseFloat(f) + parseFloat(s);
+      break;
+    case "-":
+      output = parseFloat(f) - parseFloat(s);
+      break;
+    default:
+      output = "Invalid operation";
+  }
+  return output;
+}
+(async () => {
+  const readline = createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  const first_number = await readline.question("Enter first number:");
+  const operation = await readline.question("Enter operation:(+, -, *, /):");
+  const second_number = await readline.question("Enter second number:");
+  const output = doTheMath(first_number, operation, second_number);
+  console.log("result:", output);
+  readline.close();
+})();
